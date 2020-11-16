3e62035a647a2b8fad31424f716c7e559414a14e
Gabrielė Stravinskaitė <stravinskaite.g@gmail.com>
2020-11-16 22:11:35 +0100

    Given a meal, get ingredients

--- retrieval.py
+++ retrieval.py
@@ -9,3 +9,12 @@ def give_meals(data_json, n):
     meal = random.choices(list(data.keys()), k = n)
 
     return(meal)
+
+
+def give_ingredients(data_json, meal):
+    with open(data_json) as f:
+        data = json.load(f)
+
+    return(data[meal])
+
+print(give_ingredients("meals.json", give_meals("meals.json", 1)[0]))
