441c98301798d842ab099d0798ceb42010f84593
Gabrielė Stravinskaitė <stravinskaite.g@gmail.com>
2020-11-16 21:49:36 +0100

    random meal selection function

---
+++ data-retrieval.py
@@ -0,0 +1,12 @@
+import json
+import random
+
+def give_meals(data_json, n):
+    with open(data_json) as f:
+        data = json.load(f)
+
+    meal = random.choices(list(data.keys()), k = n)
+
+    return(meal)
+
+print(give_meals("meals.json", 2))
